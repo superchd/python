b1b77056adbd3e85c3b2aa28005ec4c4faf05332
hyundae <63406434+superchd@users.noreply.github.com>
2022-07-18 17:26:24 +0900

    Update biggest_num.py
    biggest_num (2 case x)

--- programmers/biggest_num/biggest_num.py
+++ programmers/biggest_num/biggest_num.py
@@ -1,23 +1,30 @@
-from queue import Queue
+import copy
 
 def num_check(number, idx, tank, k):
-    for i in range(len(tank)):
-        if tank[i] < number[idx] and k > 0:
-            del tank[i]
-            k += 1
+    flag = 0
+    tank.append(number[idx])
+    l = len(tank)
 
+    for i in range(l):
+        if k == 0 : return tank, k
+        if len(tank) == 1: return tank, k
+    
+        if tank[-1] > tank[-2]:
+            tank.remove(tank[-2])
+            k -= 1
+            flag = 1
+                
     return tank, k
-
+    
 def solution(number, k):
     answer = ''
-    Q = Queue()
     tank = []
     idx = 0
     
-    while len(number) - 1 != idx and k > 0:
+    while len(number)  != idx and k > 0:
         tank, k = num_check(number, idx, tank, k)
         idx += 1
     
-
+    answer = "".join(tank) + number[idx:]
     
     return answer
