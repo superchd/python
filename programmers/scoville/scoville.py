4dd2a7c12ece83c14dd98b7cc5e9c6cfd3474549
hyundae <63406434+superchd@users.noreply.github.com>
2022-07-22 13:08:26 +0900

    Update scoville.py

--- programmers/scoville/scoville.py
+++ programmers/scoville/scoville.py
@@ -1,60 +1,47 @@
-answer = 0
-
-def make_food(scoville, K, depth, over):
-    global answer
-
-    # 스코빌지수가 k미만인 음식들
-    remain =[]
-    # 다음 재귀에 들어갈 리스트
-    next = []
-    
-    # 데이터 전처리
-    for s in scoville:
-        if s < K                  : remain.append(s)
-        elif s > K and depth == 0 : over.append(s)
-    
-    print(remain, over, depth)
-    # 종료조건
-    if len(remain) == 0: 
-        answer = depth
-        return depth
-    
-    # 처음만 정렬하고, 나머지 경우는 정렬할 필요없이 밀어넣자
-    if depth == 0: remain.sort(reverse = False)
-    print('remain = ', remain)
-    # remain의 크기가 2이상일때는, 가장맵지 않은 음식, 그다음 맵지않은 음식을 섞는다
-    if len(remain) > 2:
-        new_food = remain[0] + remain[1] * 2
-    elif len(remain) == 2:
-        new_food = remain[0] + remain[1] * 2
-        if new_food > K :
-            answer = depth + 1 
-            return depth + 1
-        else : 
-            next.append(new_food)
-            make_food(next, K, depth + 1, over)
-            return
-    # remain의 크기가 1일때는, 음식을 한번만 더 섞으면 끝이난다. 
-    elif len(remain) == 1:
-        return depth + 1
-    
-    # new_food가 r보다 큰 순간 삽입되므로, 리스트의 정렬이 유지된다.
-    for i in range(2, len(remain)):
-        print('new_food, remain[i]:', new_food, remain[i])
-        if new_food <= remain[i]: 
-            next.append(remain[i])
-        else:
-            next.append(remain[i])
-            next.append(new_food)
-            new_food = 0
-
-    make_food(next, K, depth + 1, over)
-
-    return
-
 def solution(scoville, K):
-    global answer
-    make_food(scoville, K, 0, [])
-    print(answer)
+    answer = 0
+    remain = []
+    over   = []
+
+
+    # 데이터 전처리, K보다 작은것과 큰것들을 나눈다
+    for s in scoville:
+        if s >= K : over.append(s)
+        else      : 
+            remain.sort(reverse = False)
+            remain.append(s)
+
+    # 종료조건, K보다 작은것들이 없으면 종료
+    while len(remain) != 0 :
+        next   = []
+        # K보다 작은원소가 2개보다 클때
+        if len(remain) > 2:
+            # 음식을 섞는다
+            new_food = remain[0] + 2 * remain[1]
+            # 제일 앞 두개 원소를 제거한다
+            remain.pop(0)
+            remain.pop(0)
+            # 섞어서 나온 음식을 리스트에 추가하고 정렬한다
+            for r in remain:
+                next.append(r)
+            next.append(new_food)
+            next.sort(reverse = False)
+        # K보다 작은원소가 2개일때
+        elif len(remain) == 2:
+            # 음식을 섞는다
+            new_food = remain[0] + 2 * remain[1]
+            if new_food >= K: return answer + 1
+            else : next.append(new_food)
+        # K보다 작은원소가 1개일때    
+        elif len(remain) == 1:
+            # 만들 수 없는 경우
+            if remain[0] < K and len(over) == 0:
+                return -1
+            # K보다 작은원소와 K보다 큰 원소를 섞으면 무조건 그 원소는 K이상
+            if remain[0] < K and len(over) > 0 :
+                return answer + 1
+        remain = next
+        print(remain)
+        answer += 1
 
     return answer
