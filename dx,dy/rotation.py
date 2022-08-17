81faee990c4332192d90245a30cd062d73e40d7b
hyundae <63406434+superchd@users.noreply.github.com>
2022-08-17 12:44:21 +0900

    Create rotation.py

---
+++ dx,dy/rotation.py
@@ -0,0 +1,24 @@
+inp = input()
+
+nx, ny = 0, 0 
+dir_num = 3
+# 시계방향
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0 ,1]
+
+for i in range(len(inp)):
+
+    #rotation direction
+    if inp[i] == 'R'      : dir_num = (dir_num - 1 + 4) % 4
+    elif inp[i] == 'L'    : dir_num = (dir_num + 1) % 4
+    else :
+        nx, ny = nx + dx[dir_num], ny + dy[dir_num]
+    print(nx, ny)
+
+'''
+시계방향
+[0, 1, 2, 3] -> [1, 2, 3, 0]
+반시계방향
+[0, 1, 2, 3] -> [3, 0, 1, 2]
+'''
+
