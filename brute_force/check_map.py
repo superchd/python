c620fc914f956aa1f968f52da97b6ae48cd25062
hyundae <63406434+superchd@users.noreply.github.com>
2022-08-17 17:00:12 +0900

    Update check_map.py

--- brute_force/check_map.py
+++ brute_force/check_map.py
@@ -5,9 +5,6 @@ def in_range(x, y, n, m):
     if 0 <= x < n and 0 <= y < m: return True
     else: return False
 
-# dx, dy테크닉
-dx = [i for i in range(1, n)]
-dy = [j for j in range(1, n)]
 nx, ny = 0, 0
 
 # 그래프 그리기
@@ -15,20 +12,14 @@ graph = []
 for _ in range(n):
     graph.append(list(map(str, input().split())))
 
-answer = 0
+cnt = 0
 path = []
 
-# 시뮬레이션
-for p in range(1, n):
-    for q in range(1, m): 
-        tx, ty = nx, ny
-        nx, ny = nx + p, ny + q
-        if in_range(nx, ny, n, m) and graph[nx][ny] != graph[tx][ty] and cnt <= 3:
-            #print(f'nx = {nx}, ny = {ny}')
-            cnt += 1
-            path.append([nx, ny])
-            pass
-if len(path) == 2:
-    print(path)
-    answer += 1
-print(answer)
+for i in range(1, n):
+    for j in range(1, m):
+        for k in range(i + 1, n):
+            for l in range(j + 1, m):
+                nx, ny = nx + k, ny + l
+                if in_range(nx, ny, n, m) and graph[nx][ny] != graph[i][j]:
+                    cnt += 1
+print(cnt)
