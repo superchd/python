36eb748df62549e073a9a8ee10032e3c2d82cc51
hyundae <63406434+superchd@users.noreply.github.com>
2022-08-17 12:46:37 +0900

    Update rotation.py
    L,R이 뒤바뀜

--- dx,dy/rotation.py
+++ dx,dy/rotation.py
@@ -9,12 +9,12 @@ dy = [0, -1, 0 ,1]
 for i in range(len(inp)):
 
     #rotation direction
-    if inp[i] == 'R'      : dir_num = (dir_num - 1 + 4) % 4
-    elif inp[i] == 'L'    : dir_num = (dir_num + 1) % 4
+    if inp[i] == 'L'      : dir_num = (dir_num - 1 + 4) % 4
+    elif inp[i] == 'R'    : dir_num = (dir_num + 1) % 4
     else :
         nx, ny = nx + dx[dir_num], ny + dy[dir_num]
-    print(nx, ny)
-
+print(nx, ny)
+ 
 '''
 시계방향
 [0, 1, 2, 3] -> [1, 2, 3, 0]
