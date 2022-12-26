381f8cdcd3470cedb93a0ea0f888fb42ec5cdd76
hyundae <63406434+superchd@users.noreply.github.com>
2022-12-26 23:55:47 +0900

    memory 초과는 고쳤음!

--- bfs/bfs_basic.py
+++ bfs/bfs_basic.py
@@ -1,42 +1,59 @@
 from collections import deque
 
-# 변수 선언 및 입력
-n, m = tuple(map(int, input().split()))
-
-a = [
+# 변수 선언 및 입력:
+n, k = tuple(map(int, input().split()))
+grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
 
 visited = [
-    [False for _ in range(m)]
+    [False for _ in range(n)]
     for _ in range(n)
-]
+    ]
+
+points = []
+for _ in range(k):
+    points.append(tuple(map(int, input().split())))
+
+def in_range(x, y):
+    if 1 <= x <= n  and 1 <= y <= n : return True
+    else : return False
+
+def can_go(x, y):
+    if grid[x - 1][y - 1] == 0 and not visited[x - 1][y - 1]:
+        return True
+    else : return False
+
+'''
+def push(x, y):
+    visited[x - 1][y - 1] = True
+    queue.append((x, y))
+'''
 
 dxs = [1, 0, -1, 0]
 dys = [0, -1, 0, 1]
 
-def in_range(x, y):
-    if 0 <= x <= n - 1 and 0 <= y <= m - 1: return True
-    else: return False
+def bfs(x, y):
+    queue = deque([(x, y)])
+    while queue:
+        curr_v = queue.popleft()
+        for dx, dy in zip(dxs, dys):
+            new_x, new_y = x + dx, y + dy
+            if in_range(new_x, new_y) and can_go(new_x, new_y):
+                print(new_x, new_y)
+                visited[new_x - 1][new_y - 1] = True
+                queue.append((new_x, new_y))
 
-def can_go(x, y):
-    if a[x][y] == 1 and not visited[x][y]: return True
-    else: return False
+for i in range(k):
+    x, y = points[i][0], points[i][1]
+    visited[x - 1][y - 1] = True
+    bfs(x, y)
 
-def push(x, y):
-    visited[x][y] = True
-    queue.append((x, y))
+cnt = 0
+for i in range(n):
+    for j in range(n):
+        if visited[i][j] == 1:
+            cnt += 1
 
-queue = deque([(0, 0)])
-
-while queue:
-    curr_v = queue.popleft()
-    x, y = curr_v[0], curr_v[1]
-    for dx, dy in zip(dxs, dys):
-        new_x, new_y = x + dx, y + dy
-        if in_range(new_x, new_y) and can_go(new_x, new_y):
-            push(new_x, new_y)
-
-if visited[n - 1][m - 1] == 1: print(1)
-else : print(0)
+print(cnt)
