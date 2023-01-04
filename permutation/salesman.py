df1b1ff27e9c28656487683e4ea405a501611e35
hyundae <63406434+superchd@users.noreply.github.com>
2023-01-04 23:06:47 +0900

    왜 perm_lst에 selected가 들어가지 않을까??

--- permutation/salesman.py
+++ permutation/salesman.py
@@ -5,22 +5,27 @@ a = [
     for _ in range(n)
 ]
 
-visited = [0 for _ in range(n)]
+visited = [0 for _ in range(n + 1)]
 
 perm_lst = []
 
 def perm(cur_idx, selected, visited):
 
-    if cur_idx == n - 1:
+    if cur_idx == n:
         perm_lst.append(selected)
-        visited = [0 for _ in range(n)]
+        visited = [0 for _ in range(n + 1)]
+        print(selected)
         return
 
-    for i in range(n):
-        if not visited[i]:
-            selected.append(i)
-            visited[i] = True
-            perm(cur_idx + 1, selected, visited)
+    for i in range(1, n + 1):
+        if visited[i]:
+            continue
+        
+        selected.append(i)
+        visited[i] = True
+        perm(cur_idx + 1, selected, visited)
+        visited[i] = False
+        selected.pop(-1)            
     
 perm(0, [], visited)
 print(perm_lst)
