3962288fe4321cad39fffd9d02c478587bfbf9c0
hyundae <63406434+superchd@users.noreply.github.com>
2023-01-05 23:27:59 +0900

    문제를잘읽자 1부터 시작한다 알겠삼?

--- permutation/salesman.py
+++ permutation/salesman.py
@@ -2,8 +2,6 @@ import copy
 import sys
 
 n = int(input())
-
-
 val = sys.maxsize
 
 a = [
@@ -17,28 +15,29 @@ perm_lst = []
 
 def perm(cur_idx, selected, visited):
 
-    if cur_idx == n:
+    if cur_idx == n - 1:
         element = copy.deepcopy(selected)
         perm_lst.append(element)
         visited = [0 for _ in range(n + 1)]
         return
 
-    for i in range(1, n + 1):
+    for i in range(2, n + 1):
         if visited[i]:
             continue
-        
         selected.append(i)
         visited[i] = True
         perm(cur_idx + 1, selected, visited)
         visited[i] = False
         selected.pop(-1)            
     
-perm(0, [], visited)
+visited[0] = True
+perm(0, [1], visited)
 
+#print(perm_lst)
 
 def count(ele):
-    ele.append(ele[0])
     cnt = 0
+    ele.append(1)
     for i in range(n):
         if a[ele[i] - 1][ele[i + 1] - 1] == 0:
             return -1
@@ -46,9 +45,9 @@ def count(ele):
     return cnt
 
 s = []
+
 for j in range(len(perm_lst)):
     if count(perm_lst[j]) != -1:
         if count(perm_lst[j]) < val:
             val = count(perm_lst[j])
-
 print(val)
