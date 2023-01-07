45282148c6210dad5f22707fdd5053d3f46d741d
hyundae <63406434+superchd@users.noreply.github.com>
2023-01-07 20:14:15 +0900

    구역을 구분하는 방법을 dfs로 구현 성공!!

--- dfs/safety_zone.py
+++ dfs/safety_zone.py
@@ -1,3 +1,5 @@
+import sys
+
 N, M = map(int, input().split())
 
 a = [
@@ -10,8 +12,6 @@ visited = [
     for _ in range(N)
 ]
 
-cnt = 0
-
 dxs = [1, 0, -1, 0]
 dys = [0, -1, 0, 1]
 
@@ -23,22 +23,10 @@ def in_range(x, y):
 def can_go(x, y, k):
     if in_range(x, y) and not visited[x][y] and a[x][y] > k :
         return True
-
-def is_blocked(x, y, k):
-    for dx, dy in zip(dxs, dys):
-        current_x, current_y = x + dx, y + dy
-        if can_go(current_x, current_y, k):
-            return False
-
-    return True
+    else: return False
 
 def dfs(current, path, k):
-    global cnt
     x, y = current[0], current[1]
-    if is_blocked(x, y, k):
-        print(path) 
-        cnt += 1
-        return
 
     for dx, dy in zip(dxs, dys):
         current_x, current_y = x + dx, y + dy
@@ -47,14 +35,25 @@ def dfs(current, path, k):
         visited[current_x][current_y] = True
         path.append((current_x, current_y))
         dfs((current_x, current_y), path, k)
-        visited[current_x][current_y] = False
-        path.pop()
 
-cnt = 0
-for i in range(N):
-    for j in range(M):
-        if can_go(i, j, 4):
-            visited[i][j] = True
-            dfs((i, j), [(i, j)], 4)
-            
-print(cnt)
+mini = -sys.maxsize
+max_value = max(map(max, a))
+max_idx = 0
+
+for k in range(max_value):
+    cnt = 0
+    for i in range(N):
+        for j in range(M):
+            if can_go(i, j, k):
+                visited[i][j] = True
+                dfs((i, j), [(i, j)], k)
+                cnt += 1
+    # initialize
+    visited = [
+        [False for _ in range(M)]
+        for _ in range(N)
+        ]
+    if cnt > mini:
+        mini = cnt
+        max_idx = k
+print(k, mini)
