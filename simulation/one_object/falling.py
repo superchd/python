6e18aa7c28c00e9b168c1352d35faee7f359f5e4
hyundae <63406434+superchd@users.noreply.github.com>
2023-01-16 01:17:58 +0900

    OMG,,, the exception happens when n == 1

--- simulation/one_object/falling.py
+++ simulation/one_object/falling.py
@@ -9,26 +9,26 @@ a = [
 
 sample = copy.deepcopy(a)
 
-def check_raw(raw, m, k):
+def next_line_blocked(raw, m, k):
     for j in range(k - 1, m + k - 1):
-        if a[raw][j] != 0:
-            return False
-    return True
+        if a[raw + 1][j] != 0:
+            #print(f'Okay Im in next_line_blocked and {j} is 1 so, it returns false')
+            return True
+    return False
 
 def paint_raw(raw, m , k):
     for j in range(k - 1, m + k - 1):
         a[raw][j] = 1
     return
 
-for r in range(n):
-    if check_raw(r, m, k) == True:
+for r in range(n - 1):
+    if next_line_blocked(r, m, k) == True:
+        print(f"Okay, next line({r + 1}) is blocked!")
         a = copy.deepcopy(sample)
         paint_raw(r, m, k)
-
+        break
 
 for i in range(n):
     for j in range(n):
         print(a[i][j], end = ' ')
     print()
-
-
