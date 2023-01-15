5a95f5eb9357956acc352e2f539621ab178e7cea
hyundae <63406434+superchd@users.noreply.github.com>
2023-01-15 22:21:55 +0900

    Update three_nums.py

--- backtracking/N개중M개고르기/three_nums.py
+++ backtracking/N개중M개고르기/three_nums.py
@@ -1,82 +1,84 @@
-from collections import deque
-
-n, k   = map(int, input().split())
+import copy
+import sys
+n = int(input())
 
 a = [
-    input().replace(" ", "")
+    input()
     for _ in range(n)
 ]
 
-r1x, r1y = map(int, input().split())
-r2x, r2y = map(int, input().split())
+cand = []
+comb = []
 
-dxs = [1, 0, -1, 0]
-dys = [0, -1, 0, 1]
-count = [
-    [0 for _ in range(n + 1)]
-    for _ in range(n + 1)
-]
+def end_test():
+    if n < 3:
+        return -1
+    else:
+        return 1
 
-visited = [
-    [0 for _ in range(n + 1)]
-    for _ in range(n + 1)
-]
-
-interrupt = [
-    [0 for _ in range(n + 1)]
-    for _ in range(n + 1)
-]
-
-queue = deque()
-
-wall = 0
-
-def in_range(x, y):
-    if 1 <= x <= n and 1 <= y <= n: return True
-    else : return False
-
-def can_go(x, y):
-    if in_range(x, y) and not visited[x][y] and interrupt[x][y] <= k:
-        return True
-    else : return False
-
-def push(x, y, val, inter):
-    global count
-    global interrupt
-    visited[x][y] = True
-    queue.append((x, y))
-    count[x][y] = val
-    interrupt[x][y] = inter
-    #print(f'I m in push, count[{x}][{y}] = {val}' )
+def find_num():
+    for i in range(n):
+        for j in range(n):
+            if ('0' <= a[i][j] <= '9'):
+                cand.append(a[i][j])
     return
 
-flag = False
+def perm(cur_idx, cnt, current):
 
-def bfs():
+    global comb
+    
+    if cur_idx == len(cand):
+        if cnt == 3:
+            ex = copy.deepcopy(current)
+            comb.append(ex)
+        return 
+    #print(f'cur_idx = {cur_idx}, cnt = {cnt}')
+    current.append(cand[cur_idx])
+    perm(cur_idx + 1, cnt + 1, current)
+    current.pop()
+    perm(cur_idx + 1, cnt, current)
 
-    global flag
-    while queue:
-        start = queue.popleft()
-        x, y = start[0], start[1]
+def find_loc(nums):
+    start = []
+    dic = {}
+    end = []
+    for i in range(n):
+        for j in range(n):
+            if a[i][j] == 'S':
+                start.append((i, j))
+            elif a[i][j] == 'E':
+                end.append((i, j))
+            elif a[i][j] in nums:
+                dic[a[i][j]] = (i, j)
+    
+    dic = sorted(dic.values(), reverse=True)
+    mid = []
+    mid.append(start[0])
+    for d in dic:
+        mid.append(d)
+    mid.append(end[0])
+    cnt = 0
+    for i in range(len(mid) - 1):
+        dx = dist(mid[i + 1], mid[i])
+        cnt += dx
+    #print(cnt)    
+    return cnt 
 
-        if x == r2x and y == r2y and interrupt[x][y] <= k:
-            flag = True
-            return
+def dist(a, b):
+    ax, ay = a[0], a[1]
+    bx, by = b[0], b[1]
+    return abs(ax - bx) + abs(ay - by)
 
-        for dx, dy in zip(dxs, dys):
-            current_x, current_y = x + dx, y + dy
-           
-            if can_go(current_x, current_y):
-                #print(f'current = {current_x}, {current_y} // {a[current_x - 1][current_y -1]}')
-                if a[current_x - 1][current_y - 1] == '1':
-                    push(current_x, current_y, count[x][y] + 1, interrupt[x][y] + 1)
-                else:
-                    push(current_x, current_y, count[x][y] + 1, interrupt[x][y])
-
-queue.append((r1x, r1y))
-bfs()
-#print(count)
-#print(interrupt)
-if flag == True:
-    print(count[r2x][r2y])
-else : print(-1)
+find_num()
+cand.sort()
+perm(0, 0, [])
+#print(comb)
+mini = sys.maxsize
+for c in comb:
+    if mini > find_loc(c):
+        mini = find_loc(c)
+    #    mini = count_val(c)
+if n < 3:
+    print(-1)
+else :
+    print(mini)
