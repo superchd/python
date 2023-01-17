1399402bc95a792ddce0d38f3deb3e8a09eee7b6
hyundae <63406434+superchd@users.noreply.github.com>
2023-01-18 00:29:59 +0900

    maximum recursion exceed

--- backtracking/N개중M개고르기/three_nums.py
+++ backtracking/N개중M개고르기/three_nums.py
@@ -8,77 +8,61 @@ a = [
 ]
 
 cand = []
-comb = []
+start = (-1, -1)
+end = (-1, -1)
 
-def end_test():
-    if n < 3:
-        return -1
-    else:
-        return 1
+ans = sys.maxsize
 
 def find_num():
+    global cand
     for i in range(n):
         for j in range(n):
             if ('0' <= a[i][j] <= '9'):
                 cand.append(a[i][j])
     return
 
-def perm(cur_idx, cnt, current):
-
-    global comb
-    
-    if cur_idx == len(cand):
-        if cnt == 3:
-            ex = copy.deepcopy(current)
-            comb.append(ex)
-        return 
-    #print(f'cur_idx = {cur_idx}, cnt = {cnt}')
-    current.append(cand[cur_idx])
-    perm(cur_idx + 1, cnt + 1, current)
-    current.pop()
-    perm(cur_idx + 1, cnt, current)
-
-def find_loc(nums):
-    start = []
-    dic = {}
-    end = []
-    for i in range(n):
-        for j in range(n):
-            if a[i][j] == 'S':
-                start.append((i, j))
-            elif a[i][j] == 'E':
-                end.append((i, j))
-            elif a[i][j] in nums:
-                dic[a[i][j]] = (i, j)
-    
-    dic = sorted(dic.values(), reverse=True)
-    mid = []
-    mid.append(start[0])
-    for d in dic:
-        mid.append(d)
-    mid.append(end[0])
-    cnt = 0
-    for i in range(len(mid) - 1):
-        dx = dist(mid[i + 1], mid[i])
-        cnt += dx
-    #print(cnt)    
-    return cnt 
-
+# 시작포인트와 끝포인트 찾아보기
+for i in range(n):
+    for j in range(n):
+        if a[i][j] == 'S':
+            start = (i, j)
+        elif a[i][j] == 'E':
+            end = (i, j)
+        
 def dist(a, b):
     ax, ay = a[0], a[1]
     bx, by = b[0], b[1]
-    return abs(ax - bx) + abs(ay - by)
+    dx = abs(ax - bx)
+    dy = abs(ay - by)
+    return dx + dy
 
-find_num()
-cand.sort()
-perm(0, 0, [])
-#print(comb)
-mini = sys.maxsize
-for c in comb:
-    if mini > find_loc(c):
-        mini = find_loc(c)
-    #    mini = count_val(c)
-if n < 3:
-    print(-1)
-else :
-    print(mini)
+def calculate_move(nums):
+    points = []
+    for i in range(n):
+        for j in range(n):
+            if a[i][j] in nums:
+                points.append((i, j))
+    
+    move_sum = dist(start, points[0])
+    for i in range(len(points) - 1):
+        move_sum += dist(points[i], points[i + 1])
+    move_sum += dist(points[-1], end)
+    return move_sum
+
+def find_min_move(cur_idx, cnt, nums):
+    global ans
+    if cur_idx == len(cand) - 1:
+        if cnt == 3:
+            if ans > calculate_move(nums):
+                ans = calculate_move
+        return
+
+    # 현재 idx를 선택했을 때
+    nums.append(cur_idx)
+    find_min_move(cur_idx + 1, cnt + 1, nums)
+    # 현재 idx를 선택하지 않고 넘어갈 때
+    nums.pop()
+    find_min_move(cur_idx + 1, cnt, nums)    
+
+find_min_move(0, 0, [])
+print(ans)
